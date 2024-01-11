ebf050c59330c6e44c405539ea562bfdb0417b55
Raphaël Doutaz <raphael.doutaz@format-z.ch>
2024-01-11 09:34:44 +0100

    feat: testing average

--- api/app.test.js
+++ api/app.test.js
@@ -4,11 +4,19 @@ const requestWithSupertest = supertest(server);
 const { describe, it, expect } = require('@jest/globals');
 
 describe('Grades Endpoints', () => {
-    it('GET /grades should show all grades', async () => {
+    it('GET /grades should show all grades & average', async () => {
         const res = await requestWithSupertest.get('/grades');
 
         expect(res.status).toEqual(200);
         expect(res.type).toEqual(expect.stringContaining('json'));
         expect(res.body).toHaveProperty('grades')
+        expect(res.body).toHaveProperty('average')
+
+        const grades = res.body.grades;
+        const average = grades.reduce((acc, grade) => {
+            return acc + grade.grade
+        }, 0) / grades.length;
+
+        expect(res.body.average).toEqual(average);
     });
 });
