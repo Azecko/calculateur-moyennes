67af19cccd78481ecd7eb2f38e47ce1e63f84b96
Raphaël Doutaz <raphael.doutaz@format-z.ch>
2024-01-23 20:35:32 +0100

    [feat] R: subject route successful

--- api/app.test.js
+++ api/app.test.js
@@ -1,20 +1,31 @@
 const server = require('./app.js');
+const mysql = require('mysql2');
 const supertest = require('supertest');
-const requestWithSupertest = supertest(server);
+const request = supertest(server);
 const { describe, it, expect } = require('@jest/globals');
 
-describe('Grades Endpoints', () => {
-    it('GET /grades should show all grades & average', async () => {
-        const res = await requestWithSupertest.get('/grades');
+const connection = mysql.createConnection({
+    host: 'db',
+    user: 'root',
+    password: 'secret',
+    database: 'calculateur_moyennes'
+  });
+
+describe('Subject Endpoints', () => {
+    it('PUT /subject should save in DB & return object with name', async () => {
+        const name = 'Test Subject';
+        const res = await request.put('/subject').send({name});
 
         expect(res.status).toEqual(200);
         expect(res.type).toEqual(expect.stringContaining('json'));
-        expect(res.body).toHaveProperty('grades')
-        expect(res.body).toHaveProperty('average')
+        expect(res.body).toHaveProperty('name');
+        expect(res.body.name).toEqual(name);
 
-        const grades = res.body.grades;
-        const average = Math.round(grades.reduce((sum, entry) => sum + entry.grade, 0) / grades.filter((entry) => entry.grade).length * 2) / 2;
-
-        expect(res.body.average).toEqual(average);
+        connection.query(
+            'SELECT * from subject WHERE name = ?', [name],
+            function(err, results) {
+                expect(results[0].name).toEqual(name);
+            }
+        );
     });
 });
