709026fc04595b3915da82c603980589f377c740
Raphaël Doutaz <raphael.doutaz@format-z.ch>
2024-01-25 07:38:49 +0100

    [feat] R: subject route, name too long

--- api/app.test.js
+++ api/app.test.js
@@ -28,4 +28,21 @@ describe('Subject Endpoints', () => {
             }
         );
     });
+
+    it('PUT /subject should return an error if subject name is too long and must not be saved in DB', async () => {
+        const name = 'Test Subject Who Is Very Too Long So The Test Should Fail Because The Name Is Too Long To Be Saved In The Database';
+        const res = await request.put('/subject').send({name});
+
+        expect(res.status).toEqual(400);
+        expect(res.type).toEqual(expect.stringContaining('json'));
+        expect(res.body).toHaveProperty('message');
+        expect(res.body.message).toEqual('The value must be at most 64 characters long');
+
+        connection.query(
+            'SELECT * from subject WHERE name = ?', [name],
+            function(err, results) {
+                expect(results.length).toEqual(0);
+            }
+        );
+    });
 });
