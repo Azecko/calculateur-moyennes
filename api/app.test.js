9aa71aeaac4c83029cb0cb9eee97f57d872decd6
Raphaël Doutaz <raphael.doutaz@format-z.ch>
2024-01-25 07:45:34 +0100

    [feat] B: DRY & Naming

--- api/app.test.js
+++ api/app.test.js
@@ -11,15 +11,19 @@ const connection = mysql.createConnection({
     database: 'calculateur_moyennes'
   });
 
+function testResponse(res, statusCode, propertyName, property) {
+    expect(res.status).toEqual(statusCode);
+    expect(res.type).toEqual(expect.stringContaining('json'));
+    expect(res.body).toHaveProperty(propertyName);
+    expect(res.body[propertyName]).toEqual(property);
+}
+
 describe('Subject Endpoints', () => {
     it('PUT /subject should save in DB & return object with name', async () => {
         const name = 'Test Subject';
         const res = await request.put('/subject').send({name});
 
-        expect(res.status).toEqual(200);
-        expect(res.type).toEqual(expect.stringContaining('json'));
-        expect(res.body).toHaveProperty('name');
-        expect(res.body.name).toEqual(name);
+        testResponse(res, 200, 'name', name);
 
         connection.query(
             'SELECT * from subject WHERE name = ?', [name],
@@ -30,13 +34,15 @@ describe('Subject Endpoints', () => {
     });
 
     it('PUT /subject should return an error if subject name is too long and must not be saved in DB', async () => {
-        const name = 'Test Subject Who Is Very Too Long So The Test Should Fail Because The Name Is Too Long To Be Saved In The Database';
+        const name = 'Test Subject Who Is Very Too Long So The Test Should Fail' +
+            'Because The Name Is Too Long To Be Saved In The Database';
         const res = await request.put('/subject').send({name});
 
-        expect(res.status).toEqual(400);
-        expect(res.type).toEqual(expect.stringContaining('json'));
-        expect(res.body).toHaveProperty('message');
-        expect(res.body.message).toEqual('The value must be at most 64 characters long');
+        testResponse(res,
+            400,
+            'message',
+            'The value must be at most 64 characters long'
+        );
 
         connection.query(
             'SELECT * from subject WHERE name = ?', [name],
